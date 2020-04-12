e683af30a4d0ed17af3b564a180dd6d4a0ab8deb
heyimleaf <luiz1223@hotmail.com>
2020-04-12 02:57:14 -0300

    Some input value issues

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -7,33 +7,156 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
   styleUrls: ['./action-sheet.page.scss'],
 })
 export class ActionSheetPage implements OnInit {
+  titulo: string;
 
-  constructor(public alertController: AlertController) { }
+  constructor(public actionSheetController: ActionSheetController,
+              public alertController: AlertController) {}
 
     async presentActionSheet() {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Albums',
+        buttons: [{
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        }, {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+          }
+        }, {
+          text: 'Play (open modal)',
+          icon: 'arrow-dropright-circle',
+          handler: () => {
+            console.log('Play clicked');
+          }
+        }, {
+          text: 'Favorite',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
+  
+
+    async presentAlertPrompt() {
       const alert = await this.alertController.create({
-        header: 'Alert',
-        subHeader: 'Subtitle',
-        message: 'This is an alert message',
+        header: 'Prompt!',
+        inputs: [
+          {
+            name: 'name1',
+            type: 'text',
+            placeholder: 'Placeholder 1'
+          },
+          {
+            name: 'name2',
+            type: 'text',
+            id: 'name2-id',
+            value: 'hello',
+            placeholder: 'Placeholder 2'
+          },
+          // multiline input.
+          {
+            name: 'paragraph',
+            id: 'paragraph',
+            type: 'textarea',
+            placeholder: 'Placeholder 3'
+          },
+          {
+            name: 'name3',
+            value: 'http://ionicframework.com',
+            type: 'url',
+            placeholder: 'Favorite site ever'
+          },
+          // input date with min & max
+          {
+            name: 'name4',
+            type: 'date',
+            min: '2017-03-01',
+            max: '2018-01-12'
+          },
+          // input date without min nor max
+          {
+            name: 'name5',
+            type: 'date'
+          },
+          {
+            name: 'name6',
+            type: 'number',
+            min: -5,
+            max: 10
+          },
+          {
+            name: 'name7',
+            type: 'number'
+          }
+        ],
         buttons: [
           {
             text: 'Cancel',
             role: 'cancel',
             cssClass: 'secondary',
             handler: () => {
-              console.log('Confirm Cancel: blah');
+              console.log('Confirm Cancel');
             }
           }, {
-            text: 'Okay',
+            text: 'Ok',
             handler: () => {
-              console.log('Confirm Okay');
+              console.log('Confirm Ok');
             }
           }
         ]
       });
+  
       await alert.present();
     }
+
+
+    async presentInput() {
+      const alert = await this.alertController.create({
+        header: 'Prompt!',
+        inputs: [
+          {
+            name: 'txtTitulo',
+            type: 'text',
+            placeholder: 'Título'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Ok',
+            handler: (data) => {
+              console.log('Confirm Ok', data);
+              this.titulo = data.txtTitulo;
+            }
+          }
+        ]
+      });
   
+      await alert.present();
+    }
+
   ngOnInit() {
   }
 
